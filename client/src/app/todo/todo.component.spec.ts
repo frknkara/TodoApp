5448c8c43dd84b015d957487e4236d4e1bb8d047
Furkan Kara <frknkara@hotmail.com>
2021-09-11 22:52:57 +0300

    add input text and add button

--- client/src/app/todo/todo.component.spec.ts
+++ client/src/app/todo/todo.component.spec.ts
@@ -50,4 +50,27 @@ describe('TodoComponent', () => {
     expect(columns[0].nativeElement.textContent).toBe('#');
     expect(columns[1].nativeElement.textContent).toBe('Item');
   });
+
+  it('should have no record found message when list is empty', () => {
+    const fixture = TestBed.createComponent(TodoComponent);
+    const instance = fixture.componentInstance;
+    spyOnProperty(instance, 'list').and.returnValue([]);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.no-record')?.textContent).toBe('No record found.');
+  });
+
+  it('should have an empty input text field', () => {
+    const fixture = TestBed.createComponent(TodoComponent);
+    let input = fixture.debugElement.query(By.css('input[type="text"]'));
+    expect(input).toBeTruthy();
+    expect(input.nativeElement.textContent).toBe('');
+  });
+
+  it('should have an add button', () => {
+    const fixture = TestBed.createComponent(TodoComponent);
+    let button = fixture.debugElement.query(By.css('button'));
+    expect(button).toBeTruthy();
+    expect(button.nativeElement.textContent).toBe('Add');
+  });
 });
--- client/src/app/todo/todo.component.ts
+++ client/src/app/todo/todo.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface TodoItem {
+  id: number,
+  item: string
+}
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
@@ -7,6 +12,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
+  private _list: Array<TodoItem> = [];
+
+  public get list() {
+    return this._list;
+  }
+  public set list(value: Array<TodoItem>) {
+    this._list = value;
+  }
   constructor() { }
 
   ngOnInit(): void {
