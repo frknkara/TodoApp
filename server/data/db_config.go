87ac3cdabfa9567b6f3435fa0c35d1a6cb1f7981
Furkan Kara <frknkara@hotmail.com>
2021-10-11 11:52:14 +0300

    docker compose implementation

--- server/data/db_config.go
+++ server/data/db_config.go
@@ -1,13 +1,23 @@
 package data
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 var (
 	dbUsername = "root"
 	dbPassword = "root"
-	dbHost     = "localhost"
+	dbHost     = getHostNameFromEnv()
 	dbPort     = "3306"
 	dbName     = "tododb"
 	dbDSN      = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		dbUsername, dbPassword, dbHost, dbPort, dbName)
 )
+
+func getHostNameFromEnv() string {
+	if value, ok := os.LookupEnv("MYSQL_HOST_NAME"); ok {
+		return value
+	}
+	return "localhost"
+}
